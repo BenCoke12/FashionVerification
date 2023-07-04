a9681883a73cb313784d5f83d55d3e54d41dcc10
Ben Coke <bc90@hw.ac.uk>
2023-07-04 16:41:52 +0100

    results from trials

--- robustnessIndividualImg.sh
+++ robustnessIndividualImg.sh
@@ -25,10 +25,10 @@ done
 #    echo "Runtime on 1l-0.01-image:$i: $((end-start))" > logs/times/log1l-0.01-$i.txt
 #done
 #epsilon 0.05
-for i in {5..99..1}
+for i in {12..99..1}
 do
     start=`date +%s`
-    timeout 3600 vehicle verify --specification vclspecs/fashionRobustness.vcl --network classifier:onnxnetworks/fashion1l.onnx --parameter epsilon:0.05 --dataset image:idxdata/individuals/Image$i.idx --dataset label:idxdata/individuals/Label$i.idx --verifier Marabou --verifierLocation ../Marabou/build/Marabou > logs/fashion1l/onelayer0.05-$i.txt && echo "Completed onelayer0.05-$i" || echo "timeout onelayer0.05$i"
+    vehicle verify --specification vclspecs/fashionRobustness.vcl --network classifier:onnxnetworks/fashion1l.onnx --parameter epsilon:0.05 --dataset image:idxdata/individuals/Image$i.idx --dataset label:idxdata/individuals/Label$i.idx --verifier Marabou --verifierLocation ../Marabou/build/Marabou > logs/fashion1l/onelayer0.05-$i.txt
     end=`date +%s`
     echo "Runtime on 1l-0.05-image:$i: $((end-start))" > logs/times/log1l-0.05-$i.txt
 done
@@ -36,7 +36,7 @@ done
 for i in {0..99..1}
 do
     start=`date +%s`
-    timeout 3600 vehicle verify --specification vclspecs/fashionRobustness.vcl --network classifier:onnxnetworks/fashion1l.onnx --parameter epsilon:0.1 --dataset image:idxdata/individuals/Image$i.idx --dataset label:idxdata/individuals/Label$i.idx --verifier Marabou --verifierLocation ../Marabou/build/Marabou > logs/fashion1l/onelayer0.1-$i.txt && echo "Completed onelayer0.1-$i" || echo "timeout onelayer0.1$i"
+    vehicle verify --specification vclspecs/fashionRobustness.vcl --network classifier:onnxnetworks/fashion1l.onnx --parameter epsilon:0.1 --dataset image:idxdata/individuals/Image$i.idx --dataset label:idxdata/individuals/Label$i.idx --verifier Marabou --verifierLocation ../Marabou/build/Marabou > logs/fashion1l/onelayer0.1-$i.txt
     end=`date +%s`
     echo "Runtime on 1l-0.1-image:$i: $((end-start))" > logs/times/log1l-0.1-$i.txt
 done
@@ -44,7 +44,7 @@ done
 for i in {0..99..1}
 do
     start=`date +%s`
-    timeout 3600 vehicle verify --specification vclspecs/fashionRobustness.vcl --network classifier:onnxnetworks/fashion1l.onnx --parameter epsilon:0.5 --dataset image:idxdata/individuals/Image$i.idx --dataset label:idxdata/individuals/Label$i.idx --verifier Marabou --verifierLocation ../Marabou/build/Marabou > logs/fashion1l/onelayer0.5-$i.txt && echo "Completed onelayer0.5-$i" || echo "timeout onelayer0.5$i"
+    vehicle verify --specification vclspecs/fashionRobustness.vcl --network classifier:onnxnetworks/fashion1l.onnx --parameter epsilon:0.5 --dataset image:idxdata/individuals/Image$i.idx --dataset label:idxdata/individuals/Label$i.idx --verifier Marabou --verifierLocation ../Marabou/build/Marabou > logs/fashion1l/onelayer0.5-$i.txt 
     end=`date +%s`
     echo "Runtime on 1l-0.5-image:$i: $((end-start))" > logs/times/log1l-0.1-$i.txt
 done
