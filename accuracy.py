2e552d02f61c3e6b9aae7872d15ddd90e1f000c3
Ben Coke <bc90@hw.ac.uk>
2023-07-09 17:13:48 +0100

    Accuracy file

---
+++ accuracy.py
@@ -0,0 +1,65 @@
+import numpy as np
+import onnxruntime as ort
+import tensorflow as tf
+from tensorflow import keras
+import onnx
+import sys
+
+#model location
+filename = sys.argv[1]
+
+#basic validation and sanity checks of the model - check onnx equiv to tf model
+onnx_model = onnx.load("./onnxnetworks/" + filename + ".onnx")
+onnx.checker.check_model(onnx_model)
+print(onnx.helper.printable_graph(onnx_model.graph))
+
+data = tf.keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+class_labels = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images/255.0
+test_images = test_images/255.0
+
+image = test_images[1].astype(np.float32)
+imageReshape = np.array([image])
+
+ortSession = ort.InferenceSession("./onnxnetworks/" + filename + ".onnx")
+ort_inputs = {ortSession.get_inputs()[0].name: imageReshape}
+onnx_result = ortSession.run(None, ort_inputs)
+
+print(onnx_result)
+print(class_labels[np.argmax(onnx_result)])
+
+#train and test accuracy
+train_correct = 0
+train_total = 0
+
+for i in range(len(train_images)):
+    image = train_images[i].astype(np.float32)
+    imageReshape = np.array([image])
+    ort_inputs = {ortSession.get_inputs()[0].name: imageReshape}
+    onnx_result = ortSession.run(None, ort_inputs)
+    if np.argmax(onnx_result) == train_labels[i]:
+        train_correct += 1
+    train_total += 1
+
+print("Train Accuracy: ", round(train_correct/train_total, 3))
+print("Correct: ", train_correct)
+print("Total: ", train_total)
+
+test_correct = 0
+test_total = 0
+
+for i in range(len(test_images)):
+    image = test_images[i].astype(np.float32)
+    imageReshape = np.array([image])
+    ort_inputs = {ortSession.get_inputs()[0].name: imageReshape}
+    onnx_result = ortSession.run(None, ort_inputs)
+    if np.argmax(onnx_result) == test_labels[i]:
+        test_correct += 1
+    test_total += 1
+
+print("Test Accuracy: ", round(test_correct/test_total, 6))
+print("Correct: ", test_correct)
+print("Total: ", test_total)
