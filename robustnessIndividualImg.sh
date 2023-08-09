268a09146d0c2d04e94782900bdfb619cfcd295e
Ben Coke <bc90@hw.ac.uk>
2023-08-09 10:22:35 +0100

    verification experiment results

--- robustnessIndividualImg.sh
+++ robustnessIndividualImg.sh
@@ -17,45 +17,45 @@ do
 done
 
 #epsilon 0.01
-for i in {0..99..1}
+for i in {0..499..1}
 do
     now=$(date +"%T")
     echo "Current time : $now"
     start=`date +%s`
-    unbuffer vehicle verify --specification vclspecs/fashionRobustness.vcl --network classifier:onnxnetworks/fashion1l32n.onnx --parameter epsilon:0.01 --dataset imageDataset:idxdata/individuals/Image$i.idx --dataset labelDataset:idxdata/individuals/Label$i.idx --verifier Marabou --verifierLocation ../Marabou/build/Marabou > logs/fashion1l32n/onelayer32n0.01-$i.txt 
+    unbuffer vehicle verify --specification vclspecs/fashionRobustness.vcl --network classifier:onnxnetworks/fashion1l32n.onnx --parameter epsilon:0.01 --dataset imageDataset:idxdata/individuals/Image$i.idx --dataset labelDataset:idxdata/individuals/Label$i.idx --verifier Marabou --verifierLocation ../Marabou/build/Marabou > logs/fashion1l32nB/onelayer32n0.01-$i.txt 
     end=`date +%s`
-    echo "Runtime on 1l32n-0.01-image:$i: $((end-start))" > logs/times/log1l32n-0.01-$i.txt
+    echo "Runtime on 1l32n-0.01-image:$i: $((end-start))" > logs/fashion1l32nB/times/log1l32n-0.01-$i.txt
 done
 
 #epsilon 0.05
-for i in {0..99..1}
+for i in {0..499..1}
 do
     now=$(date +"%T")
     echo "Current time : $now"
     start=`date +%s`
-    unbuffer vehicle verify --specification vclspecs/fashionRobustness.vcl --network classifier:onnxnetworks/fashion1l32n.onnx --parameter epsilon:0.05 --dataset imageDataset:idxdata/individuals/Image$i.idx --dataset labelDataset:idxdata/individuals/Label$i.idx --verifier Marabou --verifierLocation ../Marabou/build/Marabou > logs/fashion1l32n/onelayer32n0.05-$i.txt 
+    unbuffer vehicle verify --specification vclspecs/fashionRobustness.vcl --network classifier:onnxnetworks/fashion1l32n.onnx --parameter epsilon:0.05 --dataset imageDataset:idxdata/individuals/Image$i.idx --dataset labelDataset:idxdata/individuals/Label$i.idx --verifier Marabou --verifierLocation ../Marabou/build/Marabou > logs/fashion1l32nB/onelayer32n0.05-$i.txt 
     end=`date +%s`
-    echo "Runtime on 1l32n-0.05-image:$i: $((end-start))" > logs/times/log1l32n-0.05-$i.txt
+    echo "Runtime on 1l32n-0.05-image:$i: $((end-start))" > logs/fashion1l32nB/times/log1l32n-0.05-$i.txt
 done
 
 #epsilon 0.1
-for i in {0..99..1}
+for i in {0..499..1}
 do
     now=$(date +"%T")
     echo "Current time : $now"
     start=`date +%s`
-    unbuffer vehicle verify --specification vclspecs/fashionRobustness.vcl --network classifier:onnxnetworks/fashion1l32n.onnx --parameter epsilon:0.1 --dataset imageDataset:idxdata/individuals/Image$i.idx --dataset labelDataset:idxdata/individuals/Label$i.idx --verifier Marabou --verifierLocation ../Marabou/build/Marabou > logs/fashion1l32n/onelayer32n0.1-$i.txt
+    unbuffer vehicle verify --specification vclspecs/fashionRobustness.vcl --network classifier:onnxnetworks/fashion1l32n.onnx --parameter epsilon:0.1 --dataset imageDataset:idxdata/individuals/Image$i.idx --dataset labelDataset:idxdata/individuals/Label$i.idx --verifier Marabou --verifierLocation ../Marabou/build/Marabou > logs/fashion1l32nB/onelayer32n0.1-$i.txt
     end=`date +%s`
-    echo "Runtime on 1l32n-0.1-image:$i: $((end-start))" > logs/times/log1l32n-0.1-$i.txt
+    echo "Runtime on 1l32n-0.1-image:$i: $((end-start))" > logs/fashion1l32nB/times/log1l32n-0.1-$i.txt
 done
 
 #epsilon 0.5
-for i in {0..99..1}
+for i in {0..499..1}
 do
     now=$(date +"%T")
     echo "Current time : $now"
     start=`date +%s`
-    unbuffer vehicle verify --specification vclspecs/fashionRobustness.vcl --network classifier:onnxnetworks/fashion1l32n.onnx --parameter epsilon:0.5 --dataset imageDataset:idxdata/individuals/Image$i.idx --dataset labelDataset:idxdata/individuals/Label$i.idx --verifier Marabou --verifierLocation ../Marabou/build/Marabou > logs/fashion1l32n/onelayer32n0.5-$i.txt 
+    unbuffer vehicle verify --specification vclspecs/fashionRobustness.vcl --network classifier:onnxnetworks/fashion1l32n.onnx --parameter epsilon:0.5 --dataset imageDataset:idxdata/individuals/Image$i.idx --dataset labelDataset:idxdata/individuals/Label$i.idx --verifier Marabou --verifierLocation ../Marabou/build/Marabou > logs/fashion1l32nB/onelayer32n0.5-$i.txt 
     end=`date +%s`
-    echo "Runtime on 1l32n-0.5-image:$i: $((end-start))" > logs/times/log1l32n-0.5-$i.txt
+    echo "Runtime on 1l32n-0.5-image:$i: $((end-start))" > logs/fashion1l32nB/times/log1l32n-0.5-$i.txt
 done
