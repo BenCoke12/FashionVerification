459066231477a74b1f0d0944a9aeb64e2222310d
Ben Coke <bc90@hw.ac.uk>
2023-08-15 10:49:06 +0100

    pgdTrainedB experiments

--- tools/resultsCounter.py
+++ tools/resultsCounter.py
@@ -3,14 +3,14 @@
 
 epsilons = [0.01, 0.05, 0.1, 0.5]
 
-results = open("../results/verificationResultspgdTrainedA.csv", "w")
+results = open("../results/verificationResultspgdTrainedB.csv", "w")
 line = 'index,epsilon,outcome,time\n'
 
 for epsilon in epsilons:
     for index in range(500):
         #files
-        verificationFile = '../logs/pgdTrained/onelayer32n' + str(epsilon) + '-' + str(index) + '.txt'
-        timesFile = '../logs/pgdTrained/times/log1l32n-' + str(epsilon) + '-' + str(index) + '.txt'
+        verificationFile = '../logs/pgdTrainedB/onelayer32n' + str(epsilon) + '-' + str(index) + '.txt'
+        timesFile = '../logs/pgdTrainedB/times/log1l32n-' + str(epsilon) + '-' + str(index) + '.txt'
         print(str(index) + " : " + str(epsilon))
         #read verification file
         with open(verificationFile) as f:
